40d664ba8e81aaf3db9e9c7c47bfa68aec3054b0
Filipe Manuel Goncalves de Oliveira <filipemgoliveira94@gmail.com>
2015-05-19 15:37:23 +0100

    NodoProdutoComprado

--- src/NodoProdutoComprado.java
+++ src/NodoProdutoComprado.java
@@ -1,5 +1,6 @@
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -13,5 +14,101 @@ import java.io.Serializable;
  */
 public class NodoProdutoComprado implements Serializable{
     private String codigoP;
+    private int qtdCompN[],qtdCompP[];
+    private float valorN[],valorP[];
     
+    public NodoProdutoComprado(){
+        this.codigoP="";
+        this.qtdCompN=new int[12];
+        this.qtdCompP=new int[12];
+        this.valorN=new float[12];
+        this.valorP=new float[12];
+        
+    }
+    
+    public NodoProdutoComprado(String codigoP){
+        this.codigoP=codigoP;
+        this.qtdCompN=new int[12];
+        this.qtdCompP=new int[12];
+        this.valorN=new float[12];
+        this.valorP=new float[12];
+        
+    }
+    public NodoProdutoComprado(NodoProdutoComprado npc){
+        this.codigoP=npc.getCodigoP();
+        this.qtdCompN=npc.getQtdCompN();
+        this.qtdCompP=npc.getQtdCompP();
+        this.valorN=npc.getValorN();
+        this.valorP=npc.getValorP();
+    }
+
+    public String getCodigoP() {
+        return this.codigoP;
+    }
+
+    public int[] getQtdCompN() {
+        return this.qtdCompN;
+    }
+
+    public int[] getQtdCompP() {
+        return this.qtdCompP;
+    }
+
+    public float[] getValorN() {
+        return this.valorN;
+    }
+
+    public float[] getValorP() {
+        return this.valorP;
+    }
+
+    public void setCodigoP(String codigoP) {
+        this.codigoP = codigoP;
+    }
+
+    public void setQtdCompN(int[] qtdCompN) {
+        this.qtdCompN = qtdCompN;
+    }
+
+    public void setQtdCompP(int[] qtdCompP) {
+        this.qtdCompP = qtdCompP;
+    }
+
+    public void setValorN(float[] valorN) {
+        this.valorN = valorN;
+    }
+
+    public void setValorP(float[] valorP) {
+        this.valorP = valorP;
+    }
+    
+    public int getQtdCompMes(char modo, int mes){
+        if(modo=='P') return this.qtdCompP[mes-1];
+        else if(modo=='N') return this.qtdCompN[mes-1];
+        else return 0;
+    }
+    
+    public float getValorMes(char modo, int mes){
+        if(modo=='P') return this.valorP[mes-1];
+        else if(modo=='N') return this.valorN[mes-1];
+        else return 0;
+    }
+    
+    public void incrementaQuantidade(char modo,int mes,int qtd){
+        if(modo=='P') this.qtdCompP[mes-1]+=qtd;
+        else if(modo=='N') this.qtdCompN[mes-1]+=qtd;
+    }
+    
+    public void incrementaValor(char modo, int mes, float valorUni,int qtd){
+        if(modo=='P') this.valorP[mes-1]+=(qtd*valorUni);
+        else if(modo=='N') this.valorN[mes-1]+=(qtd*valorUni);
+    }
+    
+    public NodoProdutoComprado clone(){
+        return new NodoProdutoComprado(this);
+    }
+    
+    public int hashCode(){
+        return Arrays.hashCode(new Object[]{this.codigoP,this.qtdCompN,this.qtdCompP,this.valorN,this.valorP});
+    }
 }

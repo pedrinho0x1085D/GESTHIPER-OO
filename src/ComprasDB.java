321db128d2bbf1dc59f80f47068891cbec62486b
pedrinho0x1085D <listerstorm2009@gmail.com>
2015-05-20 20:22:09 +0100

    Inserção de compras semi completa

--- src/ComprasDB.java
+++ src/ComprasDB.java
@@ -19,27 +19,76 @@ public class ComprasDB implements IComprasDB{
     private TreeSet<Compra> compras;
     
     public ComprasDB(){
+        this.clientes=new HashMap<>();
         this.compras=new TreeSet<>(new ComparatorCompra());
+        this.produtos=new HashMap<>();
+    }
+    
+    public ComprasDB(ComprasDB other){
+        this.clientes=other.getClientes();
+        this.compras=other.getCompras();
+        this.produtos=other.getProdutos();
+    }
+    public HashMap<String,NodoCliente> getClientes(){
+        HashMap<String,NodoCliente> res=new HashMap<>();
+        for(NodoCliente nc:this.clientes.values())
+            res.put(nc.getCodigoC(),nc.clone());
+        return res;
+    }
+    
+    public TreeSet<Compra> getCompras(){
+        TreeSet<Compra> res=new TreeSet<>(new ComparatorCompra());
+        for(Compra c:this.compras)
+            res.add(c.clone());
+        return res;
+    }
+    
+    public HashMap<String,NodoProduto> getProdutos(){
+        HashMap<String,NodoProduto> res=new HashMap<>();
+        for(NodoProduto np:this.produtos.values())
+            res.put(np.getCodigoP(), np.clone());
+        return res;
+    }
+    
+    public void setClientes(HashMap<String,NodoCliente> res){
+        this.clientes=new HashMap<>();
+        for(NodoCliente nc:res.values())
+            this.clientes.put(nc.getCodigoC(),nc.clone());
+        
+    }
+    
+    public void setCompras(TreeSet<Compra> res){
+        this.compras=new TreeSet<>(new ComparatorCompra());
+        for(Compra c:res)
+            this.compras.add(c.clone());
+    }
+    
+    public void setProdutos(HashMap<String,NodoProduto> res){
+        this.produtos=new HashMap<>();
+        for(NodoProduto np:res.values())
+            this.produtos.put(np.getCodigoP(), np.clone());
     }
     
     @Override
     public void insertCodigoCliente(String codigoC) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.clientes.put(codigoC, new NodoCliente(codigoC));
     }
 
     @Override
     public void insertCodigoProduto(String codigoP) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.produtos.put(codigoP, new NodoProduto(codigoP));
     }
 
     @Override
     public void registerSale(Compra c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.clientes.get(c.getCodigoCli()).registaCompra(c.clone());
+        this.compras.add(c.clone());
+        //this.produtos.get(c.getCodigoProd()).registaCompra(c.clone());
     }
 
     @Override
     public IComprasDB clone() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new ComprasDB(this);
     }
     
     
--- src/NodoCliente.java
+++ src/NodoCliente.java
@@ -7,33 +7,48 @@ import java.util.HashMap;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Pedro Cunha
  */
-public class NodoCliente implements Serializable{
+public class NodoCliente implements Serializable {
+
     private String codigoC;
     private int compraMes[];
     private int nCompras;
-    private HashMap<String,NodoProdutoComprado> prodComprados;
+    private HashMap<String, NodoProdutoComprado> prodComprados;
 
     public NodoCliente(String codigoC, int[] compraMes, int nCompras) {
         this.codigoC = codigoC;
         this.compraMes = compraMes;
         this.nCompras = nCompras;
-        this.prodComprados=new HashMap<>();
+        this.prodComprados = new HashMap<>();
     }
-    
-    public NodoCliente(String codigoC){
-        this.codigoC=codigoC;
-        this.compraMes=new int[12];
-        this.nCompras=0;
-        this.prodComprados=new HashMap<>();
+
+    public NodoCliente(String codigoC) {
+        this.codigoC = codigoC;
+        this.compraMes = new int[12];
+        this.nCompras = 0;
+        this.prodComprados = new HashMap<>();
+    }
+
+    public NodoCliente(NodoCliente other) {
+        this.codigoC = other.getCodigoC();
+        this.compraMes = other.getCompraMes();
+        this.nCompras = other.getnCompras();
+        this.prodComprados = other.getProdComprados();
     }
 
     public String getCodigoC() {
         return this.codigoC;
+    }
+
+    public HashMap<String, NodoProdutoComprado> getProdComprados() {
+        HashMap<String, NodoProdutoComprado> res = new HashMap<>();
+        for (NodoProdutoComprado npc : this.prodComprados.values()) {
+            res.put(npc.getCodigoP(), npc.clone());
+        }
+        return res;
     }
 
     public int[] getCompraMes() {
@@ -55,8 +70,31 @@ public class NodoCliente implements Serializable{
     public void setnCompras(int nCompras) {
         this.nCompras = nCompras;
     }
-    
-    
-    
-    
+
+    public void setProdComprados(HashMap<String, NodoProdutoComprado> produtosC) {
+        this.prodComprados = new HashMap<>();
+        for (NodoProdutoComprado npc : produtosC.values()) {
+            this.prodComprados.put(npc.getCodigoP(), npc.clone());
+        }
+    }
+
+    public void registaCompra(Compra c) {
+        this.compraMes[c.getMes() - 1]++;
+        this.nCompras++;
+        this.atualizaProdutos(c.clone());
+
+    }
+
+    public void atualizaProdutos(Compra c) {
+        if (this.prodComprados.containsKey(c.getCodigoProd())) {
+            this.prodComprados.put(c.getCodigoProd(), new NodoProdutoComprado());
+        }
+        this.prodComprados.get(c.getCodigoProd()).incrementaQuantidade(c.getModo(), c.getMes(), c.getQuantidade());
+        this.prodComprados.get(c.getCodigoProd()).incrementaValor(c.getModo(), c.getMes(), c.getValorUni(), c.getQuantidade());
+    }
+
+    public NodoCliente clone() {
+        return new NodoCliente(this);
+    }
+
 }
